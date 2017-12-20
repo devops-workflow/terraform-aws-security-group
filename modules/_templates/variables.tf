14f54adea217d7bbdb9c14c20a56a10f9e279410
Steven Nemetz <snemetz@hotmail.com>
2017-12-20 05:12:09 -0800

    Add label module variables to template

--- modules/_templates/variables.tf
+++ modules/_templates/variables.tf
@@ -1,27 +1,56 @@
-#################
-# Security group
-#################
+//
+// Variables specific to module label
+//
+variable "attributes" {
+  description = "Suffix name with additional attributes (policy, role, etc.)"
+  type        = "list"
+  default     = []
+}
+variable "autoscaling_group" {
+  description = "If true, generate ASG tags map resource"
+  default     = false
+}
+variable "delimiter" {
+  description = "Delimiter to be used between `name`, `namespaces`, `attributes`, etc."
+  default     = "-"
+}
+variable "environment" {
+  description = "Environment (ex: dev, qa, stage, prod)"
+}
+variable "name" {
+  description = "Base name for resource"
+}
+variable "namespace-env" {
+  description = "Prefix name with the environment"
+  default     = true
+}
+variable "namespace-org" {
+  description = "Prefix name with the organization. If both env and org namespaces are used, format will be <org>-<env>-<name>"
+  default     = false
+}
+variable "organization" {
+  description = "Organization name"
+  default     = ""
+}
+variable "tags" {
+  description = "A map of tags to add to all resources"
+  type        = "map"
+  default     = {}
+}
+
+//
+// Module specific Variables
+//
 variable "enabled" {
   description = "Set to false to prevent the module from creating anything"
   default     = true
 }
-
 variable "vpc_id" {
   description = "ID of the VPC where to create security group"
 }
-
-variable "name" {
-  description = "Name of security group"
-}
-
 variable "description" {
   description = "Description of security group"
   default     = "Security Group managed by Terraform"
-}
-
-variable "tags" {
-  description = "A mapping of tags to assign to security group"
-  default     = {}
 }
 
 ##########

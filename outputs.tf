7b5cfead02a1ce4a52fd16c6e7bcc5f613bda4d4
Steven Nemetz <snemetz@hotmail.com>
2017-12-19 18:54:35 -0800

    Cleanup outputs. Remove wasted text in the names

--- outputs.tf
+++ outputs.tf
@@ -1,36 +1,35 @@
-output "this_security_group_id" {
+output "id" {
   description = "The ID of the security group"
   value       = "${element(concat(aws_security_group.this.*.id, list("")), 0)}"
 }
 
-output "this_security_group_vpc_id" {
+output "vpc_id" {
   description = "The VPC ID"
   value       = "${element(concat(aws_security_group.this.*.vpc_id, list("")), 0)}"
 }
 
-output "this_security_group_owner_id" {
+output "owner_id" {
   description = "The owner ID"
   value       = "${element(concat(aws_security_group.this.*.owner_id, list("")), 0)}"
 }
 
-output "this_security_group_name" {
+output "name" {
   description = "The name of the security group"
   value       = "${element(concat(aws_security_group.this.*.name, list("")), 0)}"
 }
 
-output "this_security_group_description" {
+output "description" {
   description = "The description of the security group"
   value       = "${element(concat(aws_security_group.this.*.description, list("")), 0)}"
 }
 
-//output "this_security_group_ingress" {
+//output "ingress" {
 //  description = "The ingress rules"
 //  value       = "${element(concat(aws_security_group.this.*.ingress, list("")), 0)}"
 //}
 
 
-//output "this_security_group_egress" {
+//output "egress" {
 //  description = "The egress rules"
 //    value       = "${element(concat(aws_security_group.this.*.egress, list("")), 0)"
 //}
-

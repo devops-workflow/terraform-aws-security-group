35d44adadce60e6848549ffa6c88938e64da3bb1
Steven Nemetz <snemetz@hotmail.com>
2017-12-21 04:18:41 -0800

    Add rest of outputs to disable example

--- examples/disabled/outputs.tf
+++ examples/disabled/outputs.tf
@@ -1,4 +1,45 @@
+
 output "id" {
-  description = "The ID of the security group"
   value       = "${module.complete_sg_disabled.id}"
+}
+output "vpc_id" {
+  value       = "${module.complete_sg_disabled.vpc_id}"
+}
+output "owner_id" {
+  value       = "${module.complete_sg_disabled.owner_id}"
+}
+output "name" {
+  value       = "${module.complete_sg_disabled.name}"
+}
+output "description" {
+  value       = "${module.complete_sg_disabled.description}"
+}
+output "ingress" {
+  value       = "${module.complete_sg_disabled.ingress}"
+}
+output "egress" {
+  value       = "${module.complete_sg_disabled.egress}"
+}
+
+
+output "id" {
+  value       = "${module.http_sg_disabled.id}"
+}
+output "vpc_id" {
+  value       = "${module.http_sg_disabled.vpc_id}"
+}
+output "owner_id" {
+  value       = "${module.http_sg_disabled.owner_id}"
+}
+output "name" {
+  value       = "${module.http_sg_disabled.name}"
+}
+output "description" {
+  value       = "${module.http_sg_disabled.description}"
+}
+output "ingress" {
+  value       = "${module.http_sg_disabled.ingress}"
+}
+output "egress" {
+  value       = "${module.http_sg_disabled.egress}"
 }
